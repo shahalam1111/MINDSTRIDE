2b202de79c646aa61fd5564cca69066262eb1cb8
Shah Alam <shahalam1039@gmail.com>
2025-06-09 14:32:43 +0000

    Now I want to publish this web app to deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone', // Add this line for optimal deployment
   typescript: {
     ignoreBuildErrors: true,
   },
